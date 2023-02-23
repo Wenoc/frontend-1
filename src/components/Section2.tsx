e119da2c011cb2565dfe4a3bdccc1a455f88d440
unknown <norbert.kis@tsis.edu.rs>
2023-02-23 22:27:22 +0100

    popup

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "../styles/Section2.css";
 
 function Section2() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 500) {
+        setShowPopup(true);
+        window.removeEventListener("scroll", handleScroll);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const hasSeenPopup = localStorage.getItem("seenPopup");
+    if (!hasSeenPopup) {
+      setShowPopup(true);
+      localStorage.setItem("seenPopup", "true");
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  useEffect(() => {
+    if (showPopup) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [showPopup]);
+
   return (
     <div className="section2Container">
+      {showPopup && (
+        <div className="popup">
+          <h1>Popup</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
+            temporibus?
+          </p>
+          <button onClick={() => setShowPopup(false)}>Bezaras</button>
+        </div>
+      )}
       <div className="section2">
         <div className="left">
           <p className="title">Buchen Sie den Grillkurs jetzt</p>
