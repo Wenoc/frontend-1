9b0df844a35bc1d3f7bc8365b8dac4b7e4a5ff47
unknown <norbert.kis@tsis.edu.rs>
2023-02-23 17:15:37 +0100

    navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import logo from "../assets/logo.png";
 import pdf from "../assets/pdf.png";
 import "../styles/Navbar.css";
 
+import hamburger from "../assets/hamburger.svg";
+
 function Navbar() {
   return (
     <div className="navContainer">
@@ -11,23 +13,47 @@ function Navbar() {
         <div className="logoPlace">
           <img className="logo" src={logo} alt="logo" />
         </div>
-        <a href="#">Die Metzgerei</a>
-        <a href="#">Dry Aged</a>
-        <a href="#">Fleischversand</a>
-        <a href="#">Evenets/Kurse</a>
-        <a href="#">Partyservice</a>
-        <a href="#">Tagesessen</a>
-        <a href="#">Impressum</a>
-        <a href="#">Kontant</a>
-        <div className="downloadLink">
-          <img src={pdf} alt="" />
-          <a href="#">download</a>
+        <a href="#" className="navItem">
+          Die Metzgerei
+        </a>
+        <a href="#" className="navItem">
+          Dry Aged
+        </a>
+        <a href="#" className="navItem">
+          Fleischversand
+        </a>
+        <a href="#" className="navItem">
+          Evenets/Kurse
+        </a>
+        <a href="#" className="navItem">
+          Partyservice
+        </a>
+        <a href="#" className="navItem">
+          Tagesessen
+        </a>
+        <a href="#" className="navItem">
+          Impressum
+        </a>
+        <a href="#" className="navItem">
+          Kontant
+        </a>
+        <div className="right">
+          <div className="downloadLink">
+            <img src={pdf} alt="" />
+            <a href="#">download</a>
+          </div>
+
+          <div className="hamburger">
+            <img src={hamburger} alt="" height={50} width={50} />
+          </div>
         </div>
       </div>
       <div className="opentimeContainer">
         <div className="opentime">
-          <img src={clock} alt="" />
-          <span className="bold">Opentime:</span>
+          <div className="optime">
+            <img src={clock} alt="" />
+            <span className="bold">Opentime:</span>
+          </div>
           <span>
             Di. - Fr.: 07:00-13:00 und 15:00-18:30. Sa.: 07:30 - 12:30
           </span>
